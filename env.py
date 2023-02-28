cf562b3d7354fef1466f30778449e90087ad48f1
SergiyKochenko <sergiykochenko@gmail.com>
2023-02-28 15:25:43 +0000

    connect db

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://prznjffa:ZGfQzNVrd_uBn-uV4sGQ04BeWuDNikz9@manny.db.elephantsql.com/prznjffa"
+os.environ["SECRET_KEY"]="t3^hsv5rr9n#37oh)*b9pql)ircvq3g5a*2tot#li&&h82dehs"
