bfaccf38c007d66c755c1d1946dcd9321a95b017
SergiyKochenko <sergiykochenko@gmail.com>
2023-02-28 15:45:03 +0000

    deploiment trys

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"]="postgres://prznjffa:ZGfQzNVrd_uBn-uV4sGQ04BeWuDNikz9@manny.db.elephantsql.com/prznjffa"
-os.environ["SECRET_KEY"]="t3^hsv5rr9n#37oh)*b9pql)ircvq3g5a*2tot#li&&h82dehs"
+os.environ["DATABASE_URL"] = "postgres://prznjffa:ZGfQzNVrd_uBn-uV4sGQ04BeWuDNikz9@manny.db.elephantsql.com/prznjffa"
+os.environ["SECRET_KEY"] = "t3^hsv5rr9n#37oh)*b9pql)ircvq3g5a*2tot#li&&h82dehs"
+os.environ["CLOUDINARY_URL"] = "cloudinary://788785116973719:8U9Kg-ES3PA756lv4s8J3f7g3Ig@dvbhrs1gf"
